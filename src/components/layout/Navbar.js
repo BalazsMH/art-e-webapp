f14a6c99de20c90a615caa0733dc309c00016983
Laszlo Miklos Vajay <vajaylm@gmail.com>
2021-01-11 04:19:21 +0100

    Fix logo path and Quiz typo

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { logo } from '../../images/Site-logo';
+import logo from '../../images/Site-logo.png';
 
 const Navbar = () => {
     return (
@@ -7,7 +7,7 @@ const Navbar = () => {
             <img src={logo} alt="Site Logo" /><br/>
             <div>
                 <a href="/browse">Browse</a>
-                <a href="/quiz">Types</a>
+                <a href="/quiz">Quiz</a>
                 <a href="/register-user">Registration</a>
                 <a href="/about">About</a>
             </div>

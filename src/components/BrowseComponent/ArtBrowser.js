c1114cf741cb067e44fa9e3f70b1d1ba3d26b053
BalazsMH <horvathb91@gmail.com>
2021-01-17 11:29:11 +0100

    Removed unused dependencies

--- src/components/BrowseComponent/ArtBrowser.js
+++ src/components/BrowseComponent/ArtBrowser.js
@@ -1,5 +1,4 @@
-import React, {useEffect, useState, useContext} from 'react';
-import axios from 'axios';
+import React, {useContext} from 'react';
 import ArtCard from './ArtCard';
 import styled from 'styled-components';
 import ArtSearchBox from './ArtSearchBox';
@@ -16,8 +15,6 @@ export default function ArtBrowser() {
             <div>Please wait..</div>
         )
     }
-
-    //TODO: Move data load to provider
 
     return (
         <div>

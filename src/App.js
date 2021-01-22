0e8acd98a641c29a494559630a0e67797458604d
BalazsMH <horvathb91@gmail.com>
2021-01-22 17:13:15 +0100

    User Login styled

--- src/App.js
+++ src/App.js
@@ -12,49 +12,51 @@ import {ArtDataProvider} from './components/BrowseComponent/ArtBrowserContext';
 
 function App() {
   return (
-    <Router>
+    <>
       <Navbar />
-      <div className="App">
-        <Route exact path="/">
-          <h1>Landing page</h1>
-        </Route>
-        <Route exact path="/browse">
-          <ArtDataProvider>
-            <ArtBrowser></ArtBrowser>
-          </ArtDataProvider>
-        </Route>
-        <Route exact path="/quiz">
-          <h1>Quizzes</h1>
-        </Route>
-        <Route exact path="/register-user">
-          <UserRegistration />
-        </Route>
-        <Route exact path="/login">
-          <UserLogin />
-        </Route>
-        <Route exact path="/logout">
-          <h1>Logout</h1>
-        </Route>
-        <Route exact path="/about">
-          <h1>About</h1>
-        </Route>
-        <Route exact path="/scoreboard">
-          <h1>Scoreboard</h1>
-        </Route>
-        <Route exact path="/user">
-          <Redirect to="/user/general" />
-        </Route>
-        <Route exact path="/user/general">
-          <UserGeneral />
-        </Route>
-        <Route exact path="/user/game">
-          <UserStats />
-        </Route>
-        <Route exact path="/user/security">
-          <UserSecurity />
-        </Route>
-      </div>
-    </Router>
+      <Router>
+        <div className="App">
+          <Route exact path="/">
+            <h1>Landing page</h1>
+          </Route>
+          <Route exact path="/browse">
+            <ArtDataProvider>
+              <ArtBrowser></ArtBrowser>
+            </ArtDataProvider>
+          </Route>
+          <Route exact path="/quiz">
+            <h1>Quizzes</h1>
+          </Route>
+          <Route exact path="/register-user">
+            <UserRegistration />
+          </Route>
+          <Route exact path="/login">
+            <UserLogin />
+          </Route>
+          <Route exact path="/logout">
+            <h1>Logout</h1>
+          </Route>
+          <Route exact path="/about">
+            <h1>About</h1>
+          </Route>
+          <Route exact path="/scoreboard">
+            <h1>Scoreboard</h1>
+          </Route>
+          <Route exact path="/user">
+            <Redirect to="/user/general" />
+          </Route>
+          <Route exact path="/user/general">
+            <UserGeneral />
+          </Route>
+          <Route exact path="/user/game">
+            <UserStats />
+          </Route>
+          <Route exact path="/user/security">
+            <UserSecurity />
+          </Route>
+        </div>
+      </Router>
+    </>
   );
 }
 //TODO: Handle non-existing endpoint access attempts

883ada679a7f38d19ee6b8a56cc41d819ffa12ff
BalazsMH <horvathb91@gmail.com>
2021-02-18 21:17:56 +0100

    Change to controlled registration form and correct input values not being sent.

--- src/components/user/UserRegistration.js
+++ src/components/user/UserRegistration.js
@@ -2,40 +2,68 @@ import React, { useState } from 'react';
 import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
 import { RegistrationContainer } from '../Styles.js';
+import axios from 'axios';
 
 
 const UserRegistration = () => {
     const [birthDate, setBirthDate] = useState(new Date());
-
-    let userName, password, firstName, lastName;
+    const [userName, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
     
     const handleChange = (e, targetVar) => {
         targetVar = e.target.value;
     }
 
-    const handleRegistrationSubmit = () => {
-        console.log("Registered!");
+    const handleRegistrationSubmit = (e) => {
+        // e.preventDefault();
+        console.log(userName, password, firstName, lastName, email);
+        sendNewUserData();
+        console.log("Sent new user request");
+    }
+    const sendNewUserData = () => {
+        axios({
+            method: 'POST',
+            url:'http://localhost:8080/api/register',
+            params: {userName: userName,
+                    firstName: firstName,
+                    lastName: lastName,
+                    email: email,
+                    password: password,
+                    birthDate: birthDate
+                }
+        }).then(res => {
+            console.log(res);
+        })
+        .catch(e => {
+            console.log(e);
+        })
     }
 
     return (
         <RegistrationContainer>
             <form>
                 <label>Username: </label>
-                <input onChange={(e) => handleChange(e, userName)} type="text" name="userName" placeholder="Enter your username here" />
+                <input onChange={(e) => setUsername(e.target.value)} value={userName} type="text" name="userName" placeholder="Enter your username here" />
                 <br />
                 <label>Password: </label>
-                <input onChange={(e) => handleChange(e, password)} type="password" name="pwd" placeholder="Enter your password here" />
+                <input onChange={(e) => setPassword(e.target.value)} value={password} type="password" name="pwd" placeholder="Enter your password here" />
                 <br />
                 <label>First name: </label>
-                <input onChange={(e) => handleChange(e, firstName)} type="text" name="firstName" placeholder="Enter your first name here" />
+                <input onChange={(e) => setFirstName(e.target.value)} value={firstName} type="text" name="firstName" placeholder="Enter your first name here" />
                 <br />
                 <label>Last name: </label>
-                <input onChange={(e) => handleChange(e, lastName)} type="text" name="lastName" placeholder="Enter your last name here" />
+                <input onChange={(e) => setLastName(e.target.value)} value={lastName} type="text" name="lastName" placeholder="Enter your last name here" />
                 <br />
                 <label>Date of birth: </label>
                 <DatePicker selected={birthDate} onChange={(date) => setBirthDate(date)} />
                 <br />
-                <button onClick={() => handleRegistrationSubmit}>Register</button>
+                <label>Email: </label>
+                <input onChange={(e) => setEmail(e.target.value)} value={email} type="text" name="email" placeholder="Enter your email here" />
+                <br />
+                <button type="button" onClick={(e) => handleRegistrationSubmit()}>Register</button>
             </form>
         </RegistrationContainer>
     )

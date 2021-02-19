6bde40f84b9e780d3560b83fd3dd7f4fc25de17e
Méry Dániel <merydaniel@merys-mbp.t.hu>
2021-02-19 21:47:20 +0100

    Updated: info text on art of the day hover

--- src/components/landingpage/LandingPage.js
+++ src/components/landingpage/LandingPage.js
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from 'react';
-import { QuizH1, QuizzesContainer, QuizSelector, QuizH2, QuizDetails, NextButtonPage, BackButton } from '../Styles.js';
+import React from 'react';
 import styled from 'styled-components';
 
 const LandingPage = () => {
@@ -8,6 +7,10 @@ const LandingPage = () => {
         <BackGroundImg>
         <Layer>
         <ArtOfTheDay>
+            <ArtImg src="https://upload.wikimedia.org/wikipedia/commons/2/20/Johannes_Vermeer_-_Het_melkmeisje_-_Google_Art_Project.jpg" />
+            <TextConatainer>
+            <ImgText>ART OF THE DAY<br></br>Maker:<br></br>Johannes Vermeer<br></br>Title:<br></br>The Milkmaid</ImgText>
+            </TextConatainer>
         </ArtOfTheDay>
         </Layer>
         </BackGroundImg>
@@ -24,21 +27,53 @@ const BackGroundImg = styled.div`
 const Layer = styled.div`
     min-height: 100vh;
     background-image: linear-gradient(to bottom, rgba(255, 255, 255, 0.2), rgba(255, 255, 255, 0.5));
+    display: flex;
+    justify-content: center;
+
+`;
+
+const ArtImg = styled.img`
+    width: 100%;
+    border: 1rem solid black;
+    opacity: 1;
+    display: block;
+    transition: .5s ease;
+    backface-visibility: hidden;
+`;
+
+const TextConatainer = styled.div`
+    transition: .5s ease;
+    opacity: 0;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    width: 80%;
+    transform: translate(-50%, -50%);
+    -ms-transform: translate(-50%, -50%);
+    text-align: center;
+`;
+
+const ImgText = styled.p`
+    background-color: black;
+    color: white;
+    font-size: 1.3rem;
+    padding: 1.3rem 2.6rem;
+    
+
 `;
 
 const ArtOfTheDay = styled.div`
-    position: relative;
-    margin-left: auto;
-    margin-right: auto;
-    border: 1rem solid black;
-    background-image: url("https://upload.wikimedia.org/wikipedia/commons/2/20/Johannes_Vermeer_-_Het_melkmeisje_-_Google_Art_Project.jpg");
-    background-size: cover;
+    position: absolute;
+    top: 26%;
     height: 30rem;
     width: 30rem;
-    &:hover {
-        filter: brightness(30%);
+    &:hover ${ArtImg}{
+        opacity: 0.8;
     }
-    
+    &:hover ${TextConatainer}{
+        opacity: 1;
+    }
 `;
+
 
 export default LandingPage;

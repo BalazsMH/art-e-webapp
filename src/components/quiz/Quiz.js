f8bf9dc01807e193f6128b91baee5d646306d1d4
Méry Dániel <merydaniel@merys-mbp.t.hu>
2021-03-03 23:43:48 +0100

    Add: user statisctics changes in the memory during quizzes

--- src/components/quiz/Quiz.js
+++ src/components/quiz/Quiz.js
@@ -2,15 +2,22 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import {Question} from './Question';
 import { QuizContainer } from '../Styles.js';
+import cookie from 'react-cookies';
 
 const API_URL = 'http://localhost:8080/api/quiz';
 
 const Quiz = ({type}) => {
+    const userStats = cookie.load("stats");
     const [questions, setQuestions] = useState([]);
     const [currentIndex, setCurrentIndex] = useState(0);
     const [score, setScore] = useState(0);
+    const [correctAnswers, setCorrectAnswers] = useState(userStats.correctAnswers);
+    const [allAnswers, setAllAnswers] = useState(userStats.allAnswers);
+    const [winStreak, setWinStreak] = useState(userStats.winStreak);
     const [gameEnded, setGameEnded] = useState(false);
     const [showAnswers, setShowAnswers] = useState(false);
+    let dailyRemainingXp = userStats.dailyRemainingXp;
+    let actualXp = userStats.actualXp;
     
     useEffect(() => {
         axios({
@@ -38,17 +45,28 @@ const Quiz = ({type}) => {
     const handleAnswer = (answer) => {
         if(!showAnswers) {
             if(answer === questions[currentIndex].correct_answer) {
-                setScore(score + 100)
+                setScore(score + 100);
+                setCorrectAnswers(correctAnswers + 1);
+                setWinStreak(winStreak + 1);
                 }
+            else {
+                setWinStreak(0);
             }
+            }
+        setAllAnswers(allAnswers + 1);
         setShowAnswers(true);
     }
 
     const handleNextQuestion = () => {
         setShowAnswers(false);
         setCurrentIndex(currentIndex + 1);
-
+        
         if(currentIndex + 1 >= questions.length) {
+            const dailyXpSubScore = (dailyRemainingXp - score);
+            const actuaXplPlusScore = (actualXp + score);
+            (dailyXpSubScore > 0) ? actualXp = actuaXplPlusScore : actualXp = dailyRemainingXp;
+            (dailyXpSubScore > 0) ? dailyRemainingXp = dailyXpSubScore : dailyRemainingXp = 0;
+
             setGameEnded(true);
         }
     }

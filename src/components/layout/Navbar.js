eb44b6896744cacb9bc0d4cff6c99af22fdc1a1b
BalazsMH <horvathb91@gmail.com>
2021-03-04 15:38:02 +0100

    Enable mobile view for Navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import logo from '../../images/arte.png';
 import {Avatar, Box, DropButton} from 'grommet';
 import {UserFemale, Menu} from 'grommet-icons';
-import { NavContainer, SiteLogo, NavRoutes, NavLink, UserFunctionsContainer } from '../Styles.js';
+import { NavContainer, SiteLogo, NavRoutes, NavLink, UserFunctionsContainer, MobileNav, UserDropButton, MenuDropButton, DropButtonContent } from '../Styles.js';
 import { UserInfoContext } from '../user/UserInfoContext';
 import { MediaInfoContext } from '../mediaInfo/MediaInfoContext';
 
@@ -14,6 +14,55 @@ const Navbar = () => {
 
 
     return (
+        <>
+        {isSmallScreen ?
+        <MobileNav>
+            <MenuDropButton
+            label={
+                <Menu/>
+                }
+            dropAlign={{ top: 'bottom', right: 'right' }}
+            dropContent={
+                <DropButtonContent>
+                <NavLink to="/browse">Browse</NavLink>
+                <NavLink to="/quiz">Quiz</NavLink>
+                <NavLink to="/about">About</NavLink>
+                {isLoggedIn ? <NavLink to="/favorites/0">Favorites</NavLink>
+                            : <></>}
+                </DropButtonContent>
+            }
+            />
+            <Link to="/"><SiteLogo src={logo} alt="Art-E" /></Link>
+            <UserDropButton
+                    color="plain"
+                    label={
+                    <>
+                        <Box direction="row"  gap="small">
+                            {isLoggedIn ? <Avatar src="//s.gravatar.com/avatar/b7fb138d53ba0f573212ccce38a7c43b?s=80" />
+                                        : <Avatar background="accent-2"><UserFemale color="accent-1" /></Avatar>}
+                        </Box>
+                    </>
+                        }
+                    dropAlign={{ top: 'bottom', right: 'right' }}
+                    dropContent={
+                        <>
+                        {isLoggedIn ?
+                        <>
+                            <NavLink to="/user">My Account</NavLink>
+                            <NavLink to="/logout">Logout</NavLink>
+                        </> : 
+                        <>
+                            <NavLink to="/register-user">Registration</NavLink>
+                            <NavLink to="/login">Login</NavLink>
+                        </>
+                        }
+                        </>    
+                    }
+                    />
+
+        </MobileNav>
+        
+        :
         <NavContainer>
             <Link to="/"><SiteLogo src={logo} alt="Art-E" /></Link>
             <NavRoutes>
@@ -22,9 +71,8 @@ const Navbar = () => {
                 <NavLink to="/about">About</NavLink>
                 {isLoggedIn ? <NavLink to="/favorites/0">Favorites</NavLink>
                             : <></>}
-                
                 <UserFunctionsContainer>
-                <DropButton
+                <UserDropButton
                     color="plain"
                     label={
                     <>
@@ -53,6 +101,9 @@ const Navbar = () => {
                 </UserFunctionsContainer>
             </NavRoutes>
         </NavContainer>
+        }
+        </>
+
     );
 }
 

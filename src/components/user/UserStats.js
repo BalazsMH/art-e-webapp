788271dc9a6b18c321762986031238347927570d
BalazsMH <horvathb91@gmail.com>
2021-03-12 16:00:21 +0100

    Create RankStepper component file

--- src/components/user/UserStats.js
+++ src/components/user/UserStats.js
@@ -1,23 +1,28 @@
 import React, { useContext } from 'react';
 import UserSidebar from '../layout/UserSidebar';
 import { UserStatsContext } from '../user/UserStatsContext';
+import { StatsContainer} from '../Styles.js';
+import { UserInformationContainer} from '../Styles.js';
 
 const UserStats = () => {
     const {userData, isLoaded} = useContext(UserStatsContext);
 
     return (
-        isLoaded ? (<div>
+        isLoaded ? (<UserInformationContainer>
             <UserSidebar />
-            <p>Highscores rank: { userData.rank }</p>
-            <p>XP earned: { userData.actualXp }</p>
-            <p>Daily remaining XP: { userData.dailyRemainingXp }</p>
-            <p>Correct answers: { userData.correctAnswers }</p>
-            <p>Total answers: { userData.allAnswers }</p>
-            <p>Correct answer ratio: { userData.answerRatio }</p>
-            <p>Daily streak: { userData.dailyStreak }</p>
-            <p>Win streak: { userData.winStreak }</p>
+            <StatsContainer>
+                <p>Highscores rank: { userData.rank }</p>
+                <p>XP earned: { userData.actualXp }</p>
+                <p>Daily remaining XP: { userData.dailyRemainingXp }</p>
+                <p>Correct answers: { userData.correctAnswers }</p>
+                <p>Total answers: { userData.allAnswers }</p>
+                <p>Correct answer ratio: { userData.answerRatio }</p>
+                <p>Daily streak: { userData.dailyStreak }</p>
+                <p>Win streak: { userData.winStreak }</p>
+            </StatsContainer>
+
             
-        </div>) :
+        </UserInformationContainer>) :
 
         (<h1>Loading...</h1>)
 

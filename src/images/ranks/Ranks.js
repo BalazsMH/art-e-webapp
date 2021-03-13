d3091687b9d4bd5799795aee6bfb64a6241fcfd5
BalazsMH <horvathb91@gmail.com>
2021-03-14 00:15:50 +0100

    Add svg resources to images/ranks folder.

---
+++ src/images/ranks/Ranks.js
@@ -0,0 +1,14 @@
+import {ReactComponent as Rank1} from './1.svg';
+import {ReactComponent as Rank2} from './2.svg';
+import {ReactComponent as Rank3} from './3.svg';
+import {ReactComponent as Rank4} from './4.svg';
+import {ReactComponent as Rank5} from './5.svg';
+import {ReactComponent as Rank6} from './6.svg';
+import {ReactComponent as Rank7} from './7.svg';
+import {ReactComponent as Rank8} from './8.svg';
+import {ReactComponent as Rank9} from './9.svg';
+import {ReactComponent as Rank10} from './10.svg';
+
+
+export {Rank1, Rank2, Rank3, Rank4, Rank5, Rank6, Rank7, Rank8, Rank9, Rank10};
+

263017cf4166084734e3867635388977730e3f0a
Laszlo Miklos Vajay <vajaylm@gmail.com>
2021-03-13 13:53:46 +0100

    Make FavoriteFolder droppable

--- src/components/favorites/FavoriteFolder.js
+++ src/components/favorites/FavoriteFolder.js
@@ -2,11 +2,24 @@ import React, { useState, useContext } from 'react';
 import { FolderDiv } from '../Styles.js';
 import FavoriteFolderModal from './FavoriteFolderModal';
 import { UserInfoContext } from '../user/UserInfoContext';
+import { useDrop } from 'react-dnd'
+import { ItemTypes } from '../../util/ItemTypes.js';
 
 const FavoriteFolder = (props) => {
     const { allFavFolderName } = useContext(UserInfoContext);
     const { folderName, folderColor } = props;
     const [show, setShow] = useState(false);
+    
+    const [{ canDrop, isOver }, drop] = useDrop(() => ({
+        accept: ItemTypes.CARD,
+        drop: () => ({ name: folderName }),
+        collect: (monitor) => ({
+            isOver: monitor.isOver(),
+            canDrop: monitor.canDrop(),
+        }),
+    }));
+    const isActive = canDrop && isOver;
+    let opacity = isActive ? 0.7 : canDrop ? 0.4 : 1;
     
     const showFavorites = (e) => {
         props.setFolderName(e.target.textContent)
@@ -24,6 +37,9 @@ const FavoriteFolder = (props) => {
                 colorHex={folderColor} 
                 onClick={showFavorites} 
                 onDoubleClick={() => showFolderModal(folderName)}
+                ref={drop}
+                role={folderName}
+                style={{ opacity }}
             >
                 {folderName}
             </FolderDiv>

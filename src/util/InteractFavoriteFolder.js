21ef9d3056a3d68454b50db577bcb26827c8b0ac
Laszlo Miklos Vajay <vajaylm@gmail.com>
2021-03-14 09:24:55 +0100

    Add possibility to move favorites to folders

--- src/util/InteractFavoriteFolder.js
+++ src/util/InteractFavoriteFolder.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import cookie from 'react-cookies';
+import { UserInfoContext } from '../components/user/UserInfoContext';
 
 const InteractFavoriteFolder = async (folderName, folderColor, nextFolderName) => {
     let folderUrl = `http://localhost:8080/api/favoriteFolder/addFolder/${folderName}/${folderColor}`;
@@ -20,4 +21,24 @@ const InteractFavoriteFolder = async (folderName, folderColor, nextFolderName) =
     });
 }
 
-export default InteractFavoriteFolder;
+const MoveToFavoriteFolder = async (origFolderName, newFolderName, objectName) => {
+    const allFavFolderName = "All";
+    let folderUrl = `http://localhost:8080/api/favorites/removeFromFolder/${origFolderName}/${objectName}`;
+    let method = 'DELETE';
+    if (newFolderName !== allFavFolderName) {
+        folderUrl = `http://localhost:8080/api/favorites/addFavorite/${objectName}/${newFolderName}`;
+        method = 'POST';
+    }
+    await axios({
+        method: method,
+        url: folderUrl,
+        headers: {
+            'Authorization': cookie.load("Authorization")
+        }
+    })
+    .catch(e => {
+        console.log(e);
+    });
+}
+
+export {InteractFavoriteFolder, MoveToFavoriteFolder};
